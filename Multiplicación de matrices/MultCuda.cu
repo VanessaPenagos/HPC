da06907e89c56d684129daac7970a5a84c512cc0
Nany262 <daniela-zuluaga@utp.edu.co>
2017-09-15 14:13:37 -0500

    progreso de multiplicacion

--- Multiplicación de matrices/MultCuda.cu
+++ Multiplicación de matrices/MultCuda.cu
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 
 
+__global__ void MultiplicarMatrices(float *m1, float *m2, float *mr, int n, int columna2, int fila2)
+{
+	int id = blockIdx.x*blockDim.x+threadIdx.x;
+	float resultado;
+
+	for (int i = 0; i <columna2; ++i)
+	{
+		for (int j = 0; j < fila2; ++j)
+		{
+			/* code */
+		}
+
+	}
+
+}
+
 float* LlenaMatriz(int fila,int columna, FILE *archivo, float *matriz){
 
 	for (int i = 0; i < fila; i++) {
@@ -14,8 +30,7 @@ int main(int argc, char const *argv[])
 {
 	FILE *archivo1;
 	FILE *archivo2;
-	int fila1, columna1, fila2, columna2, blockSize, gridSize;
-	float resultadop = 0;
+	int fila1, columna1, fila2, columna2, blockSize = 1024, gridSize , numOper;
 
 	// Matrices entrada Host
     float *h_m1, *h_m2;
@@ -38,6 +53,11 @@ int main(int argc, char const *argv[])
 
 		if (columna1 == fila2) {
 
+			// Número de operaciones por hacer
+			numOper = columna1*fila2;
+
+			gridSize= (int) ceil(numOper/blockSize);
+
 			// Reservando y llenado de la matriz 2
 			h_m1 = malloc((fila1*columna1)*sizeof(float*)); // Reserva memoria en el host
 			cudaMalloc(&d_m1, (fila1*columna1)); // Reserva memoria en el device
@@ -51,7 +71,7 @@ int main(int argc, char const *argv[])
 			cudaMemcpy( d_m2, h_m2, (fila2*columna2), cudaMemcpyHostToDevice); // Llenar vector-matriz en el device
   
 			// Multiplicación de matrices
-			//MultiplicarMatrices(fila1,fila2,columna2,matriz1,matriz2);
+			MultiplicarMatrices<<<gridSize, blockSize>>>(d_m1,d_m2,d_mr,numOper, columna2, fila2);
 		}
 	}
 }
