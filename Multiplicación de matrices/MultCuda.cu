63bb3e86fca14e0a097c02850a7afd1e1d46e2e6
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2017-09-22 13:54:05 -0500

    sin probar

--- Multiplicación de matrices/MultCuda.cu
+++ Multiplicación de matrices/MultCuda.cu
@@ -10,7 +10,7 @@ __global__ void MultiplicarMatrices(float *m1, float *m2, float *mr, int columna
 
     if ((fila < fila2) && (columna < columna1)) {
         for (int i = 0; i < fila2 ; i++) {
-            tmp_mult += m1[i+fila1*fila_r]*m2[i*columna2+columna_r]; 
+            tmp_mult += m1[i+columna1*fila_r]*m2[i*columna2+columna_r]; 
         }
 
         mr[fila_r*columna2+columna_r]= tmp_mult
