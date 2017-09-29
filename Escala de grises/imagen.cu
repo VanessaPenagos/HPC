437ae638d1d17dd2f3fd4dab6b5541089edb3ecc
Nany262 <daniela-zuluaga@utp.edu.co>
2017-09-29 09:24:56 -0500

    escala de grises

---
+++ Escala de grises/imagen.cu
@@ -0,0 +1,32 @@
+#include <cv.h>
+#include <cuda.h>
+
+using namespace cv;
+
+__global__ void imgchange(){
+
+	int col = blockIdx.x*blockDim.x+threadIdx.x;
+	int row = blockIdx.y*blockDim.y+threadIdx.y;
+}
+
+int main(int argc, char const *argv[])
+{
+	char *h_image, *d_image, *h_imagechange, *d_imagechange;
+
+	Mat image = imread(argv[1],0);
+
+	if (!image.empty()){
+
+		h_image = (unsigned char *)malloc()
+
+		Size s = image.size()
+		namedWindow(argv[1], WINDOW_AUTOSIZE);
+		imshow(argv[1],image)
+		return 0;
+	}
+	else
+	{
+		printf("No existe una image \n");
+	}
+}
+
