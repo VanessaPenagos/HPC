26bfd500977c250f5392a3ef0777e3e34e4cb00e
Crespiito <valencia_95_@utp.edu.co>
2017-10-06 15:20:05 -0500

    Correción, ya es funcional

--- Escala de grises/image.cu
+++ Escala de grises/image.cu
@@ -8,8 +8,8 @@ __global__ void imgGray(unsigned char * d_image, unsigned char* d_imagegray, int
     int row = blockIdx.y*blockDim.y+threadIdx.y;
     int col = blockIdx.x*blockDim.x+threadIdx.x;
 
-    if (width > col && height > row){
-        d_imagegray[row*width+col]=d_imagegray[(row*width+col)*3+2]*0.21+d_imagegray[(row*width+col)*3+1]*0.71+d_imagegray[(row*width+col)*3]*0.07;
+    if ((width > col) && (height > row)){
+        d_imagegray[row*width+col]=d_image[(row*width+col)*3+2]*0.3+d_image[(row*width+col)*3+1]*0.6+d_image[(row*width+col)*3]*0.2;
     }
 }
 
@@ -17,12 +17,12 @@ int main(int argc, char const *argv[])
 {
     uchar *h_image, *d_image, *h_imagegray, *d_imagegray;
 
-    Mat image = imread(argv[1],0);
+    Mat image = imread(argv[1],1);
     Size s = image.size();
-    int sizeRGB = s.width*s.height*image.channels(); 
-    int sizeGray = s.width*s.height;
+    int sizeRGB = s.width*s.height*image.channels()*sizeof(unsigned char);
+    int sizeGray = s.width*s.height*sizeof(unsigned char);
     int blockSize = 32;
-
+    printf("%d , %d \n",sizeRGB, sizeGray);
     if (image.empty()){
         printf("Not found the image \n");
     }
@@ -33,10 +33,10 @@ int main(int argc, char const *argv[])
 
     h_image = image.data;
 
-    cudaMemcpy(d_image,h_image,sizeGray,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_image,h_image,sizeRGB,cudaMemcpyHostToDevice);
     dim3 dimBlock(blockSize,blockSize,1);
     dim3 dimTrheads(ceil(s.width/float(blockSize)),ceil(s.height/float(blockSize)),1);
-    imgGray<<<dimBlock,dimTrheads>>>(d_image,d_imagegray,s.width,s.height);
+    imgGray<<<dimTrheads,dimBlock>>>(d_image,d_imagegray,s.width,s.height);
     cudaDeviceSynchronize();
     cudaMemcpy(h_imagegray,d_imagegray,sizeGray,cudaMemcpyDeviceToHost);
 
@@ -44,5 +44,7 @@ int main(int argc, char const *argv[])
     imageGray.create(s.height,s.width,CV_8UC1);
     imageGray.data = h_imagegray;
     imwrite("./ImageG.jpg",imageGray);
+
+    return 0;
 }
 
