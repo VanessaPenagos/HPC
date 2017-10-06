8b47a1ccfd4b252fe96a5dce9936613c638b6240
Crespiito <valencia_95_@utp.edu.co>
2017-10-06 12:04:15 -0500

    Correccion de escala de grises

--- Escala de grises/imagen.cu
+++ Escala de grises/imagen.cu
@@ -1,5 +1,6 @@
-#include <cv.h>
+#include "opencv2/opencv.hpp"
 #include <cuda.h>
+#include <stdio.h>
 
 using namespace cv;
 
@@ -14,25 +15,25 @@ __global__ void imgGray(unsigned char * d_image, unsigned char* d_imagegray, int
 
 int main(int argc, char const *argv[])
 {
-    char *h_image, *d_image, *h_imagegray, *d_imagegray;
+    uchar *h_image, *d_image, *h_imagegray, *d_imagegray;
 
     Mat image = imread(argv[1],0);
     Size s = image.size();
     int sizeRGB = s.width*s.height*image.channels(); 
     int sizeGray = s.width*s.height;
-    int blocksize = 32;
+    int blockSize = 32;
 
     if (image.empty()){
         printf("Not found the image \n");
     }
 
-    //h_image = (unsigned char*)malloc(sizeRGB);
     h_imagegray = (unsigned char*)malloc(sizeGray);
     cudaMalloc((void**)&d_image,sizeRGB);
     cudaMalloc((void**)&d_imagegray,sizeGray);
 
     h_image = image.data;
 
+    cudaMemcpy(d_image,h_image,sizeGray,cudaMemcpyHostToDevice);
     dim3 dimBlock(blockSize,blockSize,1);
     dim3 dimTrheads(ceil(s.width/float(blockSize)),ceil(s.height/float(blockSize)),1);
     imgGray<<<dimBlock,dimTrheads>>>(d_image,d_imagegray,s.width,s.height);
@@ -40,11 +41,8 @@ int main(int argc, char const *argv[])
     cudaMemcpy(h_imagegray,d_imagegray,sizeGray,cudaMemcpyDeviceToHost);
 
     Mat imageGray;
-    imageGray.create(height,width,CV_8UC1);
+    imageGray.create(s.height,s.width,CV_8UC1);
     imageGray.data = h_imagegray;
-
-    namedWindow("Gray image", WINDOW_AUTOSIZE);
-    imshow(argv[1],image);
-    return 0;
+    imwrite("./ImageG.jpg",imageGray);
 }
 
