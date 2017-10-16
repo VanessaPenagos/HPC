14c22577c3112c2a4e257b2e7f4abd12dbf0fb6d
VanessaPenagos <vanessapenagos19@gmail.com>
2017-10-16 15:13:44 -0500

    Filtro sobel secuencial con OpenCV

--- Parcial/Secuencial/filtroSobel.cpp
+++ Parcial/Secuencial/filtroSobel.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv){
   clock_t start, end;
   int scale = 1;
   int delta = 0;
-  int ddepth = CV_8UC1;
+  int ddepth = CV_16S;
 
   Mat image = imread(argv[1],1);
   Size s = image.size();
@@ -21,10 +21,10 @@ int main(int argc, char **argv){
   Mat gray_image_opencv, grad_x, grad_y, grad, abs_grad_x,abs_grad_y;
   cvtColor(image, gray_image_opencv, CV_BGR2GRAY);
 
-  Sobel( image, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  Sobel( gray_image_opencv, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
   convertScaleAbs( grad_x, abs_grad_x );
 
-  Sobel( image, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  Sobel( gray_image_opencv, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
   convertScaleAbs( grad_y, abs_grad_y );
 
   addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
