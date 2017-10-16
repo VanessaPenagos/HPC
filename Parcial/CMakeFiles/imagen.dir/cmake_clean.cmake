fba7b191303e79d3609cd04e19826967b1cd5d3a
VanessaPenagos <vanessapenagos19@gmail.com>
2017-10-16 15:14:37 -0500

    Se actualizan carpetas

--- Parcial/CMakeFiles/imagen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "imagen.pdb"
-  "imagen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imagen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
