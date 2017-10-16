9078723dff381f796259058521fdf546f0e1494a
Nany262 <daniela-zuluaga@utp.edu.co>
2017-10-16 17:38:04 -0500

    Corrección msg tiempo

--- Parcial/Secuencial/filtroSobel.cpp
+++ Parcial/Secuencial/filtroSobel.cpp
@@ -35,7 +35,8 @@ int main(int argc, char **argv){
 
   imwrite("./Sobel_Image.jpg", grad);
 
-  cout <<"Tiempo:"<<((double)(end-start))/CLOCKS_PER_SEC<<endl;
+  double time = ((double)(end-start))/CLOCKS_PER_SEC;
+  printf("Tiempo: %lf", time);
 
   return 0;
 }
