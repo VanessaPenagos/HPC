38a636498d44fe7864d00bb03f70301d80b1e4d6
VanessaPenagos <vanessapenagos19@gmail.com>
2017-10-16 14:23:29 -0500

    Modificaciones

--- Parcial/CMakeFiles/image.dir/image_generated_image.cu.o.cmake
+++ Parcial/CMakeFiles/image.dir/image_generated_image.cu.o.cmake
@@ -56,18 +56,18 @@ endif()
 
 # Set these up as variables to make reading the generated file easier
 set(CMAKE_COMMAND "/usr/bin/cmake") # path
-set(source_file "/home/users/crespiito/Hpc/HPC-1/Parcial/image.cu") # path
-set(NVCC_generated_dependency_file "/home/users/crespiito/Hpc/HPC-1/Parcial/CMakeFiles/image.dir//image_generated_image.cu.o.NVCC-depend") # path
-set(cmake_dependency_file "/home/users/crespiito/Hpc/HPC-1/Parcial/CMakeFiles/image.dir//image_generated_image.cu.o.depend") # path
+set(source_file "/home/users/vanessa19/Repositorios/HPC/Parcial/image.cu") # path
+set(NVCC_generated_dependency_file "/home/users/vanessa19/Repositorios/HPC/Parcial/CMakeFiles/image.dir//image_generated_image.cu.o.NVCC-depend") # path
+set(cmake_dependency_file "/home/users/vanessa19/Repositorios/HPC/Parcial/CMakeFiles/image.dir//image_generated_image.cu.o.depend") # path
 set(CUDA_make2cmake "/usr/share/cmake-3.5/Modules/FindCUDA/make2cmake.cmake") # path
 set(CUDA_parse_cubin "/usr/share/cmake-3.5/Modules/FindCUDA/parse_cubin.cmake") # path
 set(build_cubin OFF) # bool
 set(CUDA_HOST_COMPILER "/usr/bin/cc") # path
 # We won't actually use these variables for now, but we need to set this, in
 # order to force this file to be run again if it changes.
-set(generated_file_path "/home/users/crespiito/Hpc/HPC-1/Parcial/CMakeFiles/image.dir//.") # path
-set(generated_file_internal "/home/users/crespiito/Hpc/HPC-1/Parcial/CMakeFiles/image.dir//./image_generated_image.cu.o") # path
-set(generated_cubin_file_internal "/home/users/crespiito/Hpc/HPC-1/Parcial/CMakeFiles/image.dir//./image_generated_image.cu.o.cubin.txt") # path
+set(generated_file_path "/home/users/vanessa19/Repositorios/HPC/Parcial/CMakeFiles/image.dir//.") # path
+set(generated_file_internal "/home/users/vanessa19/Repositorios/HPC/Parcial/CMakeFiles/image.dir//./image_generated_image.cu.o") # path
+set(generated_cubin_file_internal "/home/users/vanessa19/Repositorios/HPC/Parcial/CMakeFiles/image.dir//./image_generated_image.cu.o.cubin.txt") # path
 
 set(CUDA_NVCC_EXECUTABLE "/usr/local/cuda-8.0/bin/nvcc") # path
 set(CUDA_NVCC_FLAGS -O3;-arch=sm_52 ;; ) # list
