f6ddc4bccfea284fca5a558eb9fa32b2e435e9b6
Crespiito <valencia_95_@utp.edu.co>
2017-10-20 10:29:05 -0500

    imagenes

--- Parcial/Constante/imagen.sh
+++ Parcial/Constante/imagen.sh
@@ -11,5 +11,4 @@ export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRAR
 
 export CUDA_VISIBLE_DEVICES=0
 
-./imageconstant ../Imagenes/Lenna.png
-
+./imageconstant ../Imagenes/pajaro.jpg
