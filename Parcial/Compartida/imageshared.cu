3178151c458428457470b243037d2586151a55ca
Nany262 <daniela-zuluaga@utp.edu.co>
2017-10-20 07:16:01 -0500

    Corrección de valores escala de grises

--- Parcial/Compartida/imageshared.cu
+++ Parcial/Compartida/imageshared.cu
@@ -161,12 +161,12 @@ __syncthreads();
 }
 
 __global__ void imgGray(unsigned char * d_image, unsigned char* d_imagegray, int width, int height){
-
+    
     int row = blockIdx.y*blockDim.y+threadIdx.y;
     int col = blockIdx.x*blockDim.x+threadIdx.x;
 
     if ((width > col) && (height > row)){
-        d_imagegray[row*width+col]=d_image[(row*width+col)*3+2]*0.3+d_image[(row*width+col)*3+1]*0.6+d_image[(row*width+col)*3]*0.2;
+        d_imagegray[row*width+col]=d_image[(row*width+col)*3+2]*0.299+d_image[(row*width+col)*3+1]*0.587+d_image[(row*width+col)*3]*0.114;
     }
 }
 
